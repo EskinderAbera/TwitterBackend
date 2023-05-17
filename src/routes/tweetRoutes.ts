fa908383893713e3ae840ec22096bee514f51be6
unknown <eske17.ea@gmail.com>
2023-05-17 16:53:36 +0300

    add token model

--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -49,7 +49,10 @@ router.get("/", async (req, res) => {
 // get one Tweet
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const tweet = await prisma.tweet.findUnique({ where: { id: Number(id) } });
+  const tweet = await prisma.tweet.findUnique({
+    where: { id: Number(id) },
+    include: { user: true },
+  });
   if (!tweet) {
     return res.status(404).json({ error: "Tweet don't exist!" });
   }
